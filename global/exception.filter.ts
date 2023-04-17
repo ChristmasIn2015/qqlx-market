fb046efc76f13dd015dffd820fa7bc808e07bbc2
huangwenqiang <974879409@qq.com>
2023-04-17 21:13:36 +0800

    update

--- global/exception.filter.ts
+++ global/exception.filter.ts
@@ -29,7 +29,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
                 data: null,
                 message: MAP_ENUM_ERROR_CODE.get(exception)?.text || `未知错误：${exception}`,
             };
-            this.LogRemote.log(ENUM_LOG.ERROR, request.path, chain, rest); // async
+            this.LogRemote.log(ENUM_LOG.ERROR, `${request.path}@${request.method}`, chain, rest); // async
             response.json(rest);
         }
         // 其他错误
@@ -39,7 +39,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
                 data: null,
                 message: typeof exception === "string" ? exception : exception?.message,
             };
-            this.LogRemote.log(ENUM_LOG.ERROR, request.path, chain, rest); // async
+            this.LogRemote.log(ENUM_LOG.ERROR, `${request.path}@${request.method}`, chain, rest); // async
             response.json(rest);
         }
     }
